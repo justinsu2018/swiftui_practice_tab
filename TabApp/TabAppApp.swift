457c003d1ea0a6a15295c8f7c8a72f3ba452e95b
sysu <sysu@gmail.com>
2021-09-20 23:54:11 +1000

    Initial Commit

---
+++ TabApp/TabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabAppApp.swift
+//  TabApp
+//
+//  Created by Justin Su on 20/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
